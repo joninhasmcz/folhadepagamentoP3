763388158f108d37a26ccbf9bcbfc9ca528854dd
Jonas <jonas-asa-cor@hotmail.com>
2021-03-21 23:52:21 -0300

    "Add Sindicate"

--- p3/src/com/company/Main.java
+++ p3/src/com/company/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int n1,n2;
+        int contEmployee = 0;
         int op;
         double salaryEmployee;
+
         System.out.println("Bem Vindo ao Sistema Folha de Pagamento");
         /* System.out.println("Escolha dentre as opções");
         op = input.nextInt();
@@ -16,7 +17,7 @@ public class Main {
         if(op == 1) { */
 
 
-        Employee empregado1 = new Employee("Jonas", "11315461480", 0);
+        Employee empregado1 = new Employee("Jonas", "11315461480", contEmployee, "Travessa Maria Erivânia NM 5/" );
         System.out.println(empregado1.printEmployee(empregado1));
 
 
